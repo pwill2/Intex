4fe2ced7c4cdc58086d1ae61c572b4608b1a25bc
pwill2 <williams.parker002@gmail.com>
2015-12-01 10:41:50 -0700

    Added NorthWestLabs

---
+++ NorthWestLabs/NorthWestLabs/Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using NorthWestLabs.Domain;
+using NorthWestLabs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWestLabs.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
